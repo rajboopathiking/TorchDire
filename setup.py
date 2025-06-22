215d10bee4e836e69a3f5d8b8a8b9485c2dff5a7
Boopathiraj <rajboopathiking@gmail.com>
2025-06-22 22:14:43 +0530

    add files

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=["torch>=2.0.0", "PyYAML>=6.0", "tqdm", "numpy"],
-    description="A PyTorch-based library with YAML automation",
+    description="A PyTorch-based library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourname/torchflowx",
+    url="https://github.com/rajboopathiking/TorchDire",
 )
