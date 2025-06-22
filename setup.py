f1a9e5ecc106c1878fa008abdb0c0732d08e158f
Boopathiraj <rajboopathiking@gmail.com>
2025-06-22 23:16:28 +0530

    Release: Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchdire",
-    version="0.1.0",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0,<2.3.0",
