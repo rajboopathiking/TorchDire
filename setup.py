9df2e06637bf5eef85c82045f9af4091bce4b644
Boopathiraj <rajboopathiking@gmail.com>
2025-06-22 23:23:27 +0530

    Fix: Lock numpy to <2 for compatibility with PyTorch

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchdire",
-    version="0.1.6",
+    version="0.1.8",
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0,<2.3.0",
         "PyYAML>=6.0",
         "tqdm",
-        "numpy<2",  # <-- Match pyproject.toml constraint
+        "numpy<2",           # <-- Pin numpy version here
         "matplotlib",
         "seaborn",
         "pandas",
