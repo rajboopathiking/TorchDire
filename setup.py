44ea25250d4fd12ade26ee00f92a45040283eca9
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2025-07-20 23:22:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="torchdire",
-    version="0.1.18",
+    name="TorchDire",
+    version="0.1.19",
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0,<2.3.0",
@@ -18,8 +18,18 @@ setup(
         "onnxruntime-tools",
         "timm"
     ],
-    description="A PyTorch-based library",
+    python_requires=">=3.8",
+    description="A PyTorch-based library with YAML automation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/rajboopathiking/TorchDire",
+    author="Boopathi Raj",
+    author_email="rajboopathiking@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    keywords=["pytorch", "deep learning", "automation", "yaml"]
 )
