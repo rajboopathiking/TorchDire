e3332d900965fb6942f3c0dd386fe5b949612259
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2025-12-07 10:50:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TorchDire",
-    version="0.3.1",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0,<2.3.0",
